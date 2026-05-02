a0305f3e80a03fda12354f1a9076bcf2ae5f714f
MPLine <Michelp6090@gmail.com>
2026-05-02 10:14:25 -0700

    player fire systeme

--- scenes/bullet.gd
+++ scenes/bullet.gd
@@ -1,13 +1,27 @@
 extends Area3D
 
-var speed =20
-@onready var direction = Vector3.ZERO
+@export var speed =10
+
+
+var direction
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$life_timer.start()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	#direction = speed * delta
+	global_position += direction * speed * delta
 	pass
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if not body.is_in_group("player"):
+		queue_free()
+	pass # Replace with function body.
+
+
+func _on_life_timer_timeout() -> void:
+	queue_free()
+	pass # Replace with function body.
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,14 +2,18 @@ extends CharacterBody3D
 
 
 const SPEED = 10.0
+var fire_timer = 0.1
+@export var fire_timer_rate:float
 @onready var camera = $"../Camera3D"
 @onready var bullet_scn = preload("res://scenes/bullet.tscn")
+
 var camera_posiY
 
 func _ready() -> void:
 	camera_posiY =camera.position.y
 	pass
-	
+
+#camera folow systeme
 func CameraFollow():
 	camera.position.x = lerp(camera.position.x,self.position.x,0.2)
 	camera.position.z = lerp(camera.position.z,self.position.z,0.2)
@@ -17,6 +21,7 @@ func CameraFollow():
 	
 	pass
 
+#player rotation system
 func mousrotation():
 	var mouse_pos = get_viewport().get_mouse_position()
 	var ray_origin = camera.project_ray_origin(mouse_pos)
@@ -28,36 +33,28 @@ func mousrotation():
 	if target_pos:
 		look_at(Vector3(target_pos.x, global_position.y, target_pos.z), Vector3.UP)
 
-func bullet_direction(bullet):
-	var mouse_pos = get_viewport().get_mouse_position()
-	var ray_origin = camera.project_ray_origin(mouse_pos)
-	var ray_direction = camera.project_ray_normal(mouse_pos)
-	var drop_plane = Plane(Vector3.UP, bullet.global_position.y)
-	var target_pos = drop_plane.intersects_ray(ray_origin, ray_direction)
-	return Vector3(target_pos.x, target_pos.y, target_pos.z)
-	pass
-	
-	
+
+#fire function
 func fire():
 	var g1 = bullet_scn.instantiate()
 	var g2 = bullet_scn.instantiate()
 	g1.position = $GUN1.global_position
 	g2.position = $GUN2.global_position
-	g1.direction = bullet_direction(g1)
-	g2.direction = bullet_direction(g2)
-	add_child(g1)
-	add_child(g2)
-	print("fire")
+	g1.direction = -get_global_transform().basis.z
+	g2.direction = -get_global_transform().basis.z
+	get_tree().root.add_child(g1)
+	get_tree().root.add_child(g2)
+	$anim.play("shoot")
 	pass
 	
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton and event.is_pressed():
-		print("enterfire")
-		fire()
-	pass
+
 func _physics_process(delta: float) -> void:
 	mousrotation()
 	CameraFollow()
+	if Input.is_action_pressed("fire") and fire_timer > fire_timer_rate:
+		fire()
+		fire_timer = 0.1
+	fire_timer += 0.1
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("A", "D", "W", "S")
